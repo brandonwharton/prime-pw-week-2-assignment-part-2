45bf28c4f7ae20f6e589b13d0793c1dcc88b14c3
Brandon Wharton <whartonbm@gmail.com>
2021-04-07 11:59:41 -0500

    Answered first 3 questions

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -1,6 +1,6 @@
 // WHAT IS THIS EVEN DOING?
 
-// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening. 
+// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening.
 // They are in isolation -- variables in one question do not affect other questions.
 // Describe the code as it would be run, and make sure you give us the console.log's value at the end.
 // (Don't just say 'we console log the variable' -- instead say something like 'we console log 'yes'')
@@ -26,10 +26,11 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We make a variable called name and set it's value to the string 'Dane'.
+// We then make a conditional if/else statement which checks if the boolean
+// value of our variable name against the string 'Mary'. Since name doesn't
+// equal 'Mary', the boolean value comes out as false, so our else code
+// gets ran instead, which console logs 'How do you do?'.
 
 //CODE
 /*
@@ -45,10 +46,16 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We make a variable called secret with an undefined value. We also make a
+// variable called code and set it's value to the number 123. We then write two
+// conditional if statements. The first one checks if the value of code is exactly
+// 123. Since it is, the code in the first if statement is ran, which defines
+// our secret variable as the string 'super', and then takes our code variable
+// and multiplies itself by 2. The second conditional if statement is then ran,
+// which is checking to see if the value of code is greater than 250. Since it's
+// new value is now 246, which is less than 250, the second if statement is ignored,
+// so our final console log outputs the string 'super'.
+
 
 //CODE
 /*
@@ -70,10 +77,17 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We set a variable named isStudent to the boolean value true, another named age
+// to the number 34, and third named zip to the value 55407. We then create a
+// conditional if/else if statement where the first if statement is checking the
+// compound conditional of whether both isStudent is true, and if zip is greater
+// than 80000. Since isStudent is true but zip is less than 80000, the first else
+// if statement is checked, which is checking if either isStudent is false, or
+// if age is less than 30. Since neither are true, the second else if statement
+// is checked, which is checking only whether isStudent is true. Since it is, the code
+// inside that statement gets ran, which logs 'Welcome to Prime!' to the console.
+// Since one of the conditions of the if/else if conditional has been met, the remaining
+// else statement is ignored.
 
 //CODE
 /*
@@ -97,7 +111,7 @@ if (isStudent === true && zip > 80000 ) {
 
 
 // REVERSE
-// Now, for questions 4-6, we'll give you a description and some code. 
+// Now, for questions 4-6, we'll give you a description and some code.
 // Some of the code will NOT match the description.
 // Comment above the problem code, and describe a potential solution.
 // Look closely -- things are logically incorrect or missing and wrong in the code!
@@ -106,16 +120,16 @@ if (isStudent === true && zip > 80000 ) {
 // 0. (DESCRIPTION OF CORRECT BEHAVIOR)
 // We make a variable called number and set it to 1 as a number.
 // Then we increment the number variable. Number is now 2.
-// We check if number is greater than or equal to 2. 
+// We check if number is greater than or equal to 2.
 // -- it is, so we console.log 'yes'
 
 //CODE
 /*
 let number = 1;
 
-// FIX - number-- decrements number, but the instructions ask to increment. 
+// FIX - number-- decrements number, but the instructions ask to increment.
 // Should be number++.
-number--; 
+number--;
 
 if (number >= 2) {
   console.log('yes');
@@ -125,8 +139,8 @@ if (number >= 2) {
 
 
 
-//4. We start with three variables -- colorOne is set to 'blue' and 
-// colorTwo is set to 'red', and mix is set to true. We check if mix is true 
+//4. We start with three variables -- colorOne is set to 'blue' and
+// colorTwo is set to 'red', and mix is set to true. We check if mix is true
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
@@ -140,7 +154,7 @@ if (mix === true) {
 */
 
 //5. We start with two variables -- temp is set to 40 and time is set to 4.
-// We check if temp is higher than 39 and if time is greater or equal to  4 
+// We check if temp is higher than 39 and if time is greater or equal to  4
 // -- they are so we console.log 'throw away the food!'
 
 /*
@@ -153,7 +167,7 @@ if (temp > 39 || time >= 4) {
 */
 
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
-// We check if age is greater than or equal to minAge 
+// We check if age is greater than or equal to minAge
 // -- it is, so we console.log 'enter'.
 
 /*
@@ -166,4 +180,3 @@ if(minAge <= age) {
   console.log('enter');
 }
 */
-
